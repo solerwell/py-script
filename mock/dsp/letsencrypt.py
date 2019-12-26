bf5f2715e96b810b508ac5b45524a7531e900e92
soler <solerwell@msn.com>
2019-12-26 16:51:15 +0800

    update letsencrypt.py

--- mock/dsp/letsencrypt.py
+++ mock/dsp/letsencrypt.py
@@ -5,9 +5,9 @@ from flask import request
 app = flask.Flask(__name__)
 
 
-@app.route('/.well-known/acme-challenge/6SO82AzhtpmQRakqb1vk0gdinR-ayTbtD2qYd2T2_2k', methods=['GET'])
+@app.route('/.well-known/acme-challenge/6hWWNiqvB4WkjQpQfB_5cfvO0WCXaPvswhDc-8gQJZk', methods=['GET'])
 def mock():
     print("========== Activate let's encrypt ssl =============")
     print("========== the req param is", request.args, " =============")
-    result = "6SO82AzhtpmQRakqb1vk0gdinR-ayTbtD2qYd2T2_2k.gGJ_JESggg1UIJqupOQocl8oGLmlEC52eprY90JmyQ0"
+    result = "6hWWNiqvB4WkjQpQfB_5cfvO0WCXaPvswhDc-8gQJZk.gGJ_JESggg1UIJqupOQocl8oGLmlEC52eprY90JmyQ0"
     return result
